40bfb975395605c249a211b5d1f2ef23dd2f0807
Akash Upadhyay <62694340+Spectre-ak@users.noreply.github.com>
2021-02-11 23:53:03 +0530

    Update SpaceShuttel.php

--- SpaceShuttel/SpaceShuttel.php
+++ SpaceShuttel/SpaceShuttel.php
@@ -3,9 +3,9 @@
 if ($_COOKIE['status'] == '1') {
 } else {
     echo ("
-	            <script>
-	            window.location.href = 'https://sqltry3.azurewebsites.net/uploads/loginsignup/login.php';
-	            </script>");
+        <script>
+        window.location.href = 'https://sqltry3.azurewebsites.net/uploads/loginsignup/login.php';
+        </script>");
 }
 $username = $_COOKIE['statusUsername'];
 $serverName = "xxxxxxxxxxx";
@@ -77,21 +77,21 @@ foreach ($arrrayLikes as $key => $value) {
         //   showUsersPost('$user','$orignalName','$proLink','$image','$title','$desc','$likes');
         //    ");
         echo ("
-				var title=document.getElementById('$user-title').innerHTML;
-				var desc=document.getElementById('$user-desc').innerHTML;
-				document.getElementById('$user-title').remove();
-				document.getElementById('$user-desc').remove();
-				showUsersPost('$user','$orignalName','$proLink','$image',title,desc,'$likes');               
+			var title=document.getElementById('$user-title').innerHTML;
+			var desc=document.getElementById('$user-desc').innerHTML;
+			document.getElementById('$user-title').remove();
+			document.getElementById('$user-desc').remove();
+			showUsersPost('$user','$orignalName','$proLink','$image',title,desc,'$likes');               
 	      ");
     } else {
         $proLink = "";
         $orignalName = "NASA APIs";
         echo ("
-				var title=document.getElementById('$image-title').innerHTML;
-				var desc=document.getElementById('$image-desc').innerHTML;
-				document.getElementById('$image-title').remove();
-				document.getElementById('$image-desc').remove();
-				showUsersPost('$user','$orignalName','$proLink','$image',title,desc,'$likes');               
+			var title=document.getElementById('$image-title').innerHTML;
+			var desc=document.getElementById('$image-desc').innerHTML;
+			document.getElementById('$image-title').remove();
+			document.getElementById('$image-desc').remove();
+			showUsersPost('$user','$orignalName','$proLink','$image',title,desc,'$likes');               
 	      ");
     }
 }
@@ -186,57 +186,6 @@ function showUsersPost(usernamePost, name, profile, image, titleText, descText, 
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
 	<title>Space Shuttel</title>
-	<style>
-	.image_area {
-		position: relative;
-	}
-
-	.img1 {
-		display: block;
-		max-width: 100%;
-	}
-
-	.preview {
-		overflow: hidden;
-		width: 160px;
-		height: 160px;
-		margin: 10px;
-		border: 1px solid red;
-	}
-
-	.modal-lg {
-		max-width: 1000px !important;
-	}
-
-	.overlay {
-		position: absolute;
-		bottom: 10px;
-		left: 0;
-		right: 0;
-		background-color: rgba(255, 255, 255, 0.5);
-		overflow: hidden;
-		height: 0;
-		transition: .5s ease;
-		width: 100%;
-	}
-
-	.image_area:hover .overlay {
-		height: 50%;
-		cursor: pointer;
-	}
-
-	.text {
-		color: #333;
-		font-size: 20px;
-		position: absolute;
-		top: 50%;
-		left: 50%;
-		-webkit-transform: translate(-50%, -50%);
-		-ms-transform: translate(-50%, -50%);
-		transform: translate(-50%, -50%);
-		text-align: center;
-	}
-	</style>
 </head>
 
 <body style="background-color: #131316;">
