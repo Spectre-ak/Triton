b485a055bb78a92ac51685304caf9ff2d22b00f0
Akash Upadhyay <62694340+Spectre-ak@users.noreply.github.com>
2021-02-11 21:36:33 +0530

    Update ForgotPassword.php

--- ForgotPassword.php
+++ ForgotPassword.php
@@ -1,39 +1,31 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>
-		Forgot Password
-	</title>
-	<!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
-    <link rel="shortcut icon" href="css/images/logo.png" />
-</head>
-<body  style="background-color: #131316;">
-
-
-	<div class="container">
-	<div class="container">
-		<h5 style="text-align: center; color: white;padding-top: 10%;padding-bottom: 5%;">
-		Enter your username or email and your password will be sent to your registered email</h5>
-	</div> 
-		
-		
-		<form method="POST" >
-			<div style="text-align: center;">
-				<input type="text" name="usermame" style="background-color: #191916;color: white;width: 50%;border: 2px solid #34799b;">
-			</div>
-			<div style="text-align: center;padding-top:9%;"><input type="submit" style="background-color: #0c82e2; border-radius: 10px;border: 2px solid #34799b;" name="Submit"></div>
-		</form>
-		<p id="msg" style="text-align: center;color: white;padding-top: 4%;"> </p>
-
-	 </div>
-
-
-
-
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-</body>
+   <head>
+      <title>
+         Forgot Password
+      </title>
+      <!-- Bootstrap CSS -->
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+      <link rel="shortcut icon" href="css/images/logo.png" />
+   </head>
+   <body  style="background-color: #131316;">
+      <div class="container">
+         <div class="container">
+            <h5 style="text-align: center; color: white;padding-top: 10%;padding-bottom: 5%;">
+               Enter your username or email and your password will be sent to your registered email
+            </h5>
+         </div>
+         <form method="POST" >
+            <div style="text-align: center;">
+               <input type="text" name="usermame" style="background-color: #191916;color: white;width: 50%;border: 2px solid #34799b;">
+            </div>
+            <div style="text-align: center;padding-top:9%;"><input type="submit" style="background-color: #0c82e2; border-radius: 10px;border: 2px solid #34799b;" name="Submit"></div>
+         </form>
+         <p id="msg" style="text-align: center;color: white;padding-top: 4%;"> </p>
+      </div>
+      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+   </body>
 </html>
