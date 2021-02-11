7634e67cfa78d649c3a1669e26680839b65e98d6
Akash Upadhyay <62694340+Spectre-ak@users.noreply.github.com>
2021-02-11 18:25:15 +0530

    Add files via upload

---
+++ azure/About.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+
+<head>
+
+<link rel="shortcut icon" href="css/images/logo.png" />
+
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+    
+
+   
+<title>About Us</title>    
+<style>
+body{
+color: white;
+background: linear-gradient(-45deg, #e600e9, #0275dd) no-repeat center center fixed;
+background-size: 100%;
+-ms-overflow-style: none;
+  scrollbar-width: none;
+margin-left: 2%;
+margin-right: 2%;
+}
+body::-webkit-scrollbar {
+    display: none;
+}
+h1{
+font-size: 400%; 
+padding: 1%;
+text-align: center;
+
+
+}
+.ul{
+font-size: 150%;
+margin: 5%;
+padding: 1%;
+
+font-size: large;
+
+}
+li{
+padding: 1%;
+
+font-size: 100%;
+}
+
+.jq{
+width: 1.5%;
+}
+
+@media only screen and (max-width: 1081px) {
+    .jq{
+            width:6%;
+        }
+        
+}   
+
+</style>
+</head>
+
+<body>
+<h1>Triton</h1>
+<div class="container">
+    <ul class=".ul">
+            <li>TritonEXPLORE SPACE. Triton is a social media network for space enthusiasts. Though this site is for everyone  but Triton focuses primarily on Space and related activities. On Triton you'll always be updated about all space related news, discoveries, facts from across the globe.</il>
+            <li>Triton is a platform where future astronut come to get inspired.</li>
+           
+    <li>Tech Stack Used </li>
+    
+    <ul>
+            <li>    <i class="fab fa-html5" style="color: #f16528;"></i> &nbsp&nbsp HTML    </li>
+            <li>    <i class="fab fa-css3-alt" ></i>&nbsp&nbsp&nbspCSS  </li>
+            <li>    <i class="fab fa-js" style="color: #e9ca32;"> </i>&nbsp&nbsp Java Script</li>
+            <li>    <i class="fab fa-php"></i> &nbsp&nbsp  PHP  </li>
+            <li>    <img class="jq" src="css/images/bootstrap.png"> &nbsp Bootstrap </li>
+            <li>    <img class="jq" src="css/images/image.png"/>  jQuery    </li>
+            <li>    <img class="jq" src="css/images/firebase.png"/>  Firebase   </li>
+            
+    </ul>
+    
+    <li>Checkout <i class="fab fa-github"></i>&nbsp&nbsp github for code<a href="https://github.com/Spectre-ak/Triton" style="color:white">  <i class="fas fa-external-link-alt"></i> </a>  </li>
+      
+     
+     
+    
+    
+      </ul>
+      
+      
+      
+    <h4>Developers</h4>
+        
+    <p>Akash Upadhyay</p>
+    <a>Hi I'm a student and programmer.</a>
+    <a><a style="color:white;" href="https://www.linkedin.com/in/akash-upadhyay-a565271ba/">LinkedIn &nbsp<i class="fab fa-linkedin"></i></a>,&nbsp </a> 
+    <a><a style="color:white;" href="https://github.com/Spectre-ak">Github &nbsp<i class="fab fa-github"></i></a></a> 
+      
+    <br>
+       
+    <p>Nikhil Anand</p>
+    <a><a style="color:white;" href="https://www.linkedin.com/in/nikhil-anand-1957621b1">LinkedIn &nbsp<i class="fab fa-linkedin"></i></a>,&nbsp </a> 
+    <a><a style="color:white;" href="https://github.com/Spectre-ak">Github &nbsp<i class="fab fa-github"></i></a></a> 
+    
+   
+  
+</div>
+   
+   
+      
+
+</body>
