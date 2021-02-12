807e9746a1ccc79315156cb40dc580b6f3bcea3d
Akash Upadhyay <62694340+Spectre-ak@users.noreply.github.com>
2021-02-12 14:35:29 +0530

    Add files via upload

---
+++ profile/cropImg.php
@@ -0,0 +1,264 @@
+<?php
+if($_COOKIE['status']=='1'){
+        //echo ("<script>alert('debug echo');</script>");
+        $username=$_COOKIE['statusUsername'];
+        $serverName = "xxxxxxxxxxx";
+		$connectionOptions = array("Database" => "xxxxxxxxxxxx", 
+			"Uid" => "xxxxxxxxxxxxx", 
+			"PWD" => "xxxxxxxxxxxxxxxxx");
+        $conn = sqlsrv_connect($serverName, $connectionOptions) or die( print_r( sqlsrv_errors(), true));
+
+        $tsql= "SELECT * FROM dbo.allUsersInfo";
+        $getResults= sqlsrv_query($conn, $tsql);
+
+        if ($getResults == FALSE){
+           echo ("<script>alert('Some Error occured while profile loading');</script>");
+           exit();
+        }
+        ///$tableName="";
+        $name="";
+        $location="";
+        $profile_picture_link="";
+        $cover_photo_link="";
+        
+        
+        while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)){
+           if($row['username']==$username || $row['email']==$username){
+                $name=$row['orignalName'];
+                $profile_picture_link=$row['profilePhoto'];
+                 $cover_photo_link=$row['coverPhoto'];
+                 $location=$row['location'];
+                 break;
+           }
+        }
+        echo("
+            <script>
+              window.onload=function(){
+               document.getElementById('uploaded_image').src='$profile_picture_link';
+                
+               }
+            </script>");
+            
+            
+      }
+      
+      else{
+        echo("
+            <script>
+            window.location.href = 'https://sqltry3.azurewebsites.net/uploads/loginsignup/login.php';
+            </script>");
+      }
+	  
+	  ?>
+
+
+
+<html>
+	<head>
+		<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    
+	
+		<title>ProfileImage</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>        
+		<link rel="stylesheet" href="https://unpkg.com/dropzone/dist/dropzone.css" />
+		<link href="https://unpkg.com/cropperjs/dist/cropper.css" rel="stylesheet"/>
+		<script src="https://unpkg.com/dropzone"></script>
+		<script src="https://unpkg.com/cropperjs"></script>
+		<link rel="shortcut icon" href="css/images/logo.png" />
+		<style>
+
+		.image_area {
+		  position: relative;
+		}
+
+		img {
+		  	display: block;
+		  	max-width: 100%;
+		}
+
+		.preview {
+  			overflow: hidden;
+  			width: 160px; 
+  			height: 160px;
+  			margin: 10px;
+  			border: 1px solid red;
+		}
+
+		.modal-lg{
+  			max-width: 1000px !important;
+		}
+
+		.overlay {
+		  position: absolute;
+		  bottom: 10px;
+		  left: 0;
+		  right: 0;
+		  background-color: rgba(255, 255, 255, 0.5);
+		  overflow: hidden;
+		  height: 0;
+		  transition: .5s ease;
+		  width: 100%;
+		}
+
+		.image_area:hover .overlay {
+		  height: 50%;
+		  cursor: pointer;
+		}
+
+		.text {
+		  color: #333;
+		  font-size: 20px;
+		  position: absolute;
+		  top: 50%;
+		  left: 50%;
+		  -webkit-transform: translate(-50%, -50%);
+		  -ms-transform: translate(-50%, -50%);
+		  transform: translate(-50%, -50%);
+		  text-align: center;
+		}
+		
+		</style>
+	</head>
+	<body style="background-color: #131316;" id="bodyID">
+		<div align="center" class="container" style="text-align:center">
+			<img id="logo" src="css/images/cover.png" width="200px" style="border-radius: 40px;">
+		</div>
+		
+		<div class="container" align="center">
+			
+
+			<br />
+			
+			<div class="row">
+				<div class="col-md-4">&nbsp;</div>
+				<div class="col-md-4">
+					<div class="image_area">
+						<form method="post">
+							<label for="upload_image">
+								<img src="css/images/profile_picture.png"  id="uploaded_image" class="img-responsive img-circle" />
+								<div  align="center" style="color:white">
+									Click image to Change Profile Image
+								</div>
+								<input type="file" name="image" class="image" id="upload_image" style="display:none" />
+							</label>
+						</form>
+					</div>
+			    </div>
+    		<div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true" style="background-color: #131316;" >
+			  	<div class="modal-dialog modal-lg" role="document">
+			    	<div class="modal-content" style="background-color: #131316;">
+			      		<div class="modal-header">
+			        		<h5 class="modal-title" style="padding-top:4px; color:white;">Crop Image Before Upload</h5>
+			        		<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+			          			<span aria-hidden="true" styke="color:white;">×</span> 
+			        		</button>
+			      		</div>
+			      		<div class="modal-body" style="background-color: #131316;" >
+			        		<div class="img-container">
+			            		<div class="row" style="background-color: #131316;"> 
+			                		<div class="col-md-8">
+			                    		<img src="" id="sample_image" />
+			                		</div>
+			                		<div class="col-md-4">
+			                    		<div class="preview"></div>
+			                		</div>
+			            		</div>
+			        		</div>
+			      		</div>
+						 <p align="right" id="wait" style="display:none" >Wait...</p>
+			      		<div class="modal-footer">
+							
+			      			<button type="button" id="crop" onclick="showWait();" class="btn btn-primary">Crop</button>
+			        		<button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+			      		</div>
+			    	</div>
+			  	</div>
+			</div>			
+		</div>
+		<button onclick="goBack();" class="btn btn-primary">Done</button>
+	</body>
+</html>
+
+<script>
+function showWait(){
+	document.getElementById("wait").style.display="block";
+}
+function goBack(){
+	window.location.href = 'https://sqltry3.azurewebsites.net/uploads/loginsignup/update.php';
+           
+}
+$(document).ready(function(){
+
+	var $modal = $('#modal');
+
+	var image = document.getElementById('sample_image');
+
+	var cropper;
+
+	$('#upload_image').change(function(event){
+		var files = event.target.files;
+
+		var done = function(url){
+			image.src = url;
+			$modal.modal('show');
+		};
+
+		if(files && files.length > 0)
+		{
+			reader = new FileReader();
+			reader.onload = function(event)
+			{
+				done(reader.result);
+			};
+			reader.readAsDataURL(files[0]);
+		}
+	}); 
+
+	$modal.on('shown.bs.modal', function() {
+		cropper = new Cropper(image, {
+			aspectRatio: 1,
+			viewMode: 3,
+			preview:'.preview'
+		});
+	}).on('hidden.bs.modal', function(){
+		cropper.destroy();
+   		cropper = null;
+	});
+
+	$('#crop').click(function(){
+		canvas = cropper.getCroppedCanvas({
+			width:400,
+			height:400
+		});
+
+		canvas.toBlob(function(blob){
+			url = URL.createObjectURL(blob);
+			var reader = new FileReader();
+			reader.readAsDataURL(blob);
+			reader.onloadend = function(){
+				var base64data = reader.result;
+				$.ajax({
+					url:'upl.php',
+					method:'POST',
+					data:{image:base64data},
+					success:function(data)
+					{
+						$modal.modal('hide');
+						//location.reload();
+						window.location.href = 'https://sqltry3.azurewebsites.net/uploads/loginsignup/cropImg.php';
+           
+						//$('#uploaded_image').attr('src', "uploads/1611250779.jpg");
+					}
+				});
+			};
+		});
+	});
+	
+});
+</script>
+
+
---
+++ profile/profile.php
@@ -0,0 +1,304 @@
+<?php
+  
+    
+      if($_COOKIE['status']=='1'){
+        //echo ("<script>alert('debug echo');</script>");
+        $username=$_COOKIE['statusUsername'];
+        $serverName = "xxxxxxxxxxx";
+        $connectionOptions = array("Database" => "xxxxxxxxxxxx", 
+          "Uid" => "xxxxxxxxxxxxx", 
+          "PWD" => "xxxxxxxxxxxxxxxxx");
+        $conn = sqlsrv_connect($serverName, $connectionOptions) or die( print_r( sqlsrv_errors(), true));
+
+        $tsql= "SELECT * FROM dbo.allUsersInfo";
+        $getResults= sqlsrv_query($conn, $tsql);
+
+        if ($getResults == FALSE){
+           echo ("<script>alert('Some Error occured while profile loading');</script>");
+           exit();
+        }
+        ///$tableName="";
+        $name="";
+        $followers=0;
+        $followings=0;
+        $posts=0;
+        $likedPosts=0;
+        $totalComments=0;
+        $location="";
+        $profile_picture_link="";
+        $cover_photo_link="";
+        
+        
+        while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)){
+           if($row['username']==$username || $row['email']==$username){
+                $name=$row['orignalName'];
+                $followers=$row['followers'];
+                 $followings=$row['followings'];
+                 $posts=$row['userPosts'];
+                 $likedPosts=$row['likedPosts'];
+                 $totalComments=$row['totalComments'];
+                 $profile_picture_link=$row['profilePhoto'];
+                 $cover_photo_link=$row['coverPhoto'];
+                 $location=$row['location'];
+                 break;
+           }
+        }
+        
+        
+        
+        //echo ("<script>alert('$name');</script>");
+        if($name==""){
+          setcookie('status',"",time() + (1),'/','sqltry3.azurewebsites.net');
+          setcookie('statusUsername',"",time() + (1),'/','sqltry3.azurewebsites.net');
+          echo("
+            <script>
+            window.location.href = 'https://sqltry3.azurewebsites.net/uploads/loginsignup/login.php';
+            </script>");
+        }
+            
+        echo("
+        <script>
+       	window.onload=function(){
+         
+        ");
+        
+        echo("
+          document.getElementById('Followers').innerHTML='$followers';
+           document.getElementById('FollowingID').innerHTML='$followings';
+                
+                
+                
+            document.getElementById('coverImgId').src='$cover_photo_link';
+                
+            document.getElementById('profileImgId').src='$profile_picture_link';
+           
+           document.getElementById('likedpostsID').innerHTML='$likedPosts';
+                
+           document.getElementById('postsID').innerHTML='$posts';
+                
+          ");
+          
+         
+        echo("document.getElementById('nameID').innerHTML=");
+        echo(" \"$name\" ");echo(";"); 
+        
+        echo("document.getElementById('locationid').innerHTML=");
+        echo(" \"$location\" ");
+        
+        
+        
+        echo("}
+        </script>");
+    
+            }
+      
+      else{
+        echo("
+            <script>
+            window.location.href = 'https://sqltry3.azurewebsites.net/uploads/loginsignup/login.php';
+            </script>");
+      }
+
+
+  if(isset($_POST['logout'])){
+    setcookie('status',"",time() + (1),'/','sqltry3.azurewebsites.net');
+    setcookie('statusUsername',"",time() + (1),'/','sqltry3.azurewebsites.net');
+    echo("
+        <script>
+        alert('fpormmetjt');
+        </script>");
+  }
+
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+    
+    <!--<link rel="stylesheet" href="css/profile.css">-->
+    <link rel="shortcut icon" href="css/images/logo.png" />
+    <title>Profile</title>
+  </head> 
+  <body style="background-color: #131316;" id="bodyID">
+
+
+<style>
+	.nav-link { 
+            color: white; 
+        } 
+  
+        .nav-item>a:hover { 
+        	
+            color: #0c82e2; 
+        } 
+  
+        /*code to change background color*/ 
+        .navbar-nav>.active>a { 
+            background-color: green; 
+            color: green; 
+        }
+</style>
+
+<div class="container" style="padding-top: 4px;padding-bottom: 10px; text-align: center;" >
+  <img id="logo" src="css/images/cover.png" width="200px" style="border-radius: 40px;">
+</div> 
+
+<div class="container" >
+	<ul class="nav nav-tabs nav-justified">
+        <li class="nav-item" style="background-color: #131316;">
+          <a class="nav-link" href="#peter" 
+           role="tab" data-toggle="tab" id="1" onclick="window.location.href = 'https://sqltry3.azurewebsites.net/uploads/loginsignup/SpaceShuttel.php';"><i class="fa fa-rocket" style="color: #0c82e2"></i> Space Shuttel</a>
+        </li>
+        <li class="nav-item" style="background-color: #131316">
+          <a class="nav-link" href="#danny" role="tab"
+            data-toggle="tab" id="2" onclick="window.location.href = 'https://sqltry3.azurewebsites.net/uploads/loginsignup/ExtraterrestrialLives.php';"><i class="fa fa-users" aria-hidden="true"></i> Astronauts</a>
+        </li>
+        <li class="nav-item" style="background-color: #131316" >
+          <a class="nav-link" href="#alberto" role="tab"
+            data-toggle="tab" id="4" onclick="window.location.href = 'https://sqltry3.azurewebsites.net/uploads/loginsignup/Radio.php';"><i class="fa fa-comments" aria-hidden="true"></i> Radio</a>
+        </li>
+        <li class="nav-item" style="background-color: #131316" >
+          <a class="nav-link active" href="#agumbe"role="tab" 
+            data-toggle="tab" id="3" onclick="window.location.href = 'https://sqltry3.azurewebsites.net/uploads/loginsignup/profile.php';"><i class="fa fa-user" aria-hidden="true"></i> Profile</a>
+        </li>
+    </ul>
+</div>
+
+<br>
+
+ 
+
+
+  <div class="container" id="mainContentDiv">
+ 
+    <div style="text-align: center; ">
+      <div class="container">
+        <!-- <img src="C:\Users\upadh\Desktop\app\uploads\cover.png"> -->
+        <img src="" id="coverImgId" class="img-fluid" alt="Responsive image" style="max-height:400px;max-width:80%;">
+        <div style="text-align: center;margin-top: -14%">
+          <img src="" id="profileImgId" class="img-fluid" alt="Responsive image" 
+          height="25%" width="25%" style=" max-heigth:4px; border-radius:50% 50% 50% 50%;">
+        </div>   
+      </div>   
+    </div>
+
+    <div class="container1">
+      <br>
+      <p>
+        <a id="nameID">..., </a><i  class="fa fa-map-marker" aria-hidden="true" style="color: #0c82e2;padding-left: 4px;"></i><a style="padding-left: 4px;"  id="locationid"> ...</a>
+      </p>
+      <p>
+        <a>Followers <a id="Followers">...</a>, </a>  <a>  Following  <a id="FollowingID">...</a> </a>
+      </p>
+      <p id="fakeID">Your Posts <a id="postsID">...</a>,</p> 
+      <p id="fakeIDLiked">Liked Posts <a id="likedpostsID">...</a>,</p>
+      
+      <button class="btn btn-primary" onclick="logUserOut();">Log out </button>
+      <span  style="padding: 4px; padding-top: 4px;">
+        <button class="btn btn-primary" onclick="editUserProfile();" >Edit Profile </button>
+      </span>
+      
+    </div>
+
+    
+
+</div>
+
+
+
+
+<script>
+  function editUserProfile() {
+      window.location.href="./update.php"
+  }
+  
+  function logUserOut(){
+    var aa="<form style='display:none;' method='POST' action='https://sqltry3.azurewebsites.net/uploads/loginsignup/login.php'><input id='buttonforSubmit' type='submit' name='logout'></form>";
+    document.getElementById("bodyID").innerHTML=aa;
+    document.getElementById("buttonforSubmit").click();
+  }
+
+                  
+// Defining event listener function
+function displayWindowSize(){
+    // Get width and height of the window excluding scrollbars
+    var w = window.innerWidth;
+    var h = window.innerHeight;
+    
+    // Display result inside a div element
+   ///document.getElementById("result").innerHTML = "Width: " + w + ", " + "Height: " + h;
+    //alert(w+" "+h);
+   document.getElementById("mainContentDiv").style.height=h-59+"px";
+   //document.getElementById("mainContentDiv").style.maxHeight=h+"";
+  if(w<765){
+  	document.getElementById("1").innerHTML="<i class='fa fa-rocket' style='color: #0c82e2'></i>";
+	document.getElementById("2").innerHTML="<i class='fa fa-users' style='color: #0c82e2'></i>";
+	document.getElementById("3").innerHTML="<i class='fa fa-user' style='color: #0c82e2'></i>";
+	document.getElementById("4").innerHTML="<i class='fa fa-comments' style='color: #0c82e2'></i>";
+	document.getElementById("logo").style.width="100px";
+	
+  }
+  else{
+  	document.getElementById("1").innerHTML="<i class='fa fa-rocket' style='color: #0c82e2'></i> Space Shuttel";
+	document.getElementById("2").innerHTML="<i class='fa fa-users' style='color: #0c82e2'></i> Astronauts";
+	document.getElementById("3").innerHTML="<i class='fa fa-user' style='color: #0c82e2'></i> Profile";
+	document.getElementById("4").innerHTML="<i class='fa fa-comments' style='color: #0c82e2'></i> Radio";
+	document.getElementById("logo").style.width="200px"; 
+  }
+}
+    
+// Attaching the event listener function to window's resize event
+window.addEventListener("resize", displayWindowSize);
+
+// Calling the function for the first time
+displayWindowSize();
+</script>
+<style>
+	#mainContentDiv{
+		color: white;
+		width: 100%;
+		height: 912px;
+		
+		/*border: 2px solid #34799b;*/
+		border-radius: 29px;
+		overflow-x: hidden; 
+   		 overflow-y: auto
+	}
+	/* width */
+::-webkit-scrollbar {
+  width: 3px;
+  border-radius: 10px;
+  opacity: 0.0;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: #0b0b0c00;; 
+   border-radius: 10px;
+}
+ 
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #6c757d; 
+   border-radius: 10px;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #555; 
+}
+
+</style>
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
