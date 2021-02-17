6316910868bb216a222cecc11de3ee1c2e6b1b6f
Akash Upadhyay <62694340+Spectre-ak@users.noreply.github.com>
2021-02-18 00:15:00 +0530

    Update SpaceShuttelUploadingPostScript.php

--- SpaceShuttel/SpaceShuttelUploadingPostScript.php
+++ SpaceShuttel/SpaceShuttelUploadingPostScript.php
@@ -19,16 +19,17 @@ $target_file = 'uploads/posts' . $username . $noOfPosts . '.jpg';
 $target_fileC = 'posts' . $username . $noOfPosts . '.jpg';
 $title = $_POST['title'];
 $desc = $_POST['desc'];
+$date=$_POST['date'];
 if ($ext == 'PNG' || $ext == 'png') {
     $target_file_temp = 'uploads/posts' . $username . $noOfPosts . '.png';
     if (move_uploaded_file($data_img, $target_file_temp)) {
         $img = imagecreatefrompng($target_file_temp);
         imagejpeg($img, $target_file, 60);
         unlink($target_file_temp);
-        $data_posts[$target_fileC] = array("likes" => 0, "title" => $title, "desc" => $desc, "user" => $username);
+        $data_posts[$target_fileC] = array("likes" => 0, "title" => $title, "desc" => $desc, "user" => $username,"date"=>$date);
         $json_object1 = json_encode($data_posts);
         file_put_contents('networkPosts.json', $json_object1);
-        $data[$username]['UserPosts'][$target_fileC] = array("likes" => 0, "title" => $title, "desc" => $desc);
+        $data[$username]['UserPosts'][$target_fileC] = array("likes" => 0, "title" => $title, "desc" => $desc,"date"=>$date);
         $json_object = json_encode($data);
         file_put_contents('networksJSON.json', $json_object);
         $returnObject['image'] = $target_fileC;
@@ -41,10 +42,10 @@ if ($ext == 'PNG' || $ext == 'png') {
 } else {
     $image = imagecreatefromjpeg($data_img);
     imagejpeg($image, $target_file, 60);
-    $data_posts[$target_fileC] = array("likes" => 0, "title" => $title, "desc" => $desc, "user" => $username);
+    $data_posts[$target_fileC] = array("likes" => 0, "title" => $title, "desc" => $desc, "user" => $username,"date"=>$date);
     $json_object1 = json_encode($data_posts);
     file_put_contents('networkPosts.json', $json_object1);
-    $data[$username]['UserPosts'][$target_fileC] = array("likes" => 0, "title" => $title, "desc" => $desc);
+    $data[$username]['UserPosts'][$target_fileC] = array("likes" => 0, "title" => $title, "desc" => $desc,"date"=>$date);
     $json_object = json_encode($data);
     file_put_contents('networksJSON.json', $json_object);
     $returnObject = array();
