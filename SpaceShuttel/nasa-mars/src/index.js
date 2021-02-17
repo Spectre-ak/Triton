8aac25df36c12dde77d83e47b52f4b0515c04136
Akash Upadhyay <62694340+Spectre-ak@users.noreply.github.com>
2021-02-17 23:47:56 +0530

    Add files via upload

---
+++ SpaceShuttel/nasa-mars/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import NasaApi from './NasaApiComponent.js';
+
+ReactDOM.render(
+  <NasaApi/>,
+  document.getElementById('root')
+);
